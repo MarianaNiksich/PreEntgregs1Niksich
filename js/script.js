35d7cba08af2452599165b4101b95dca73143abb
MarianaNiksich <niksichmariana01@gmail.com>
2023-03-11 16:06:03 -0300

    creando repositorio primera ent5rega javascript

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function radiacion(Kv, mAS) {
+    alert("La dosis de radiación recomendada es " + Kv + "Kv y " + mAS + "mAs")
+}
+
+let menu = "Seleccione una parte del cuerpo a radiografiar\n1- Cráneo\n2- Columna cervical\n3- Columna dorsal\n4- Columna lumbar\n5- Sacro\n6- Pelvis\n7- Femur\n8- Pierna\n9- Pie\n10- Brazo\n11- Antebrazo\n12- Mano\n0- Para salir"
+
+let salir = 0
+
+do {
+    alert(menu)
+    opcion = Number(prompt("Ingrese una opcion"))
+    if (opcion === 1) {
+        radiacion(80, 18)
+        break
+    } else if (opcion === 2) {
+        radiacion(75, 10)
+        break
+    } else if (opcion === 3) {
+        radiacion(80, 12)
+        break
+    } else if (opcion === 4) {
+        radiacion(80, 15)
+        break
+    } else if (opcion === 5) {
+        radiacion(80, 15)
+        break
+    } else if (opcion === 6) {
+        radiacion(80, 15)
+        break
+    } else if (opcion === 7) {
+        radiacion(75, 12)
+        break
+    } else if (opcion === 8) {
+        radiacion(70, 6)
+        break
+    } else if (opcion === 9) {
+        radiacion(70, 2)
+        break
+    } else if (opcion === 10) {
+        radiacion(70, 6)
+    } else if (opcion === 11) {
+        radiacion(62, 6)
+        break
+    } else if (opcion === 12) {
+        radiacion(62, 3)
+        break
+    }
+} while (opcion != 0)
